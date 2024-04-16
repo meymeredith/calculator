e02dbb4fc17c7d10b7be738f2f722c3162cb3fd9
ImyaLuboe <meredith.artem@gmail.com>
2024-04-16 13:26:30 +0300

    dopustim

---
+++ Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше выражение: ");
+            string myOps = Console.ReadLine();
+            Console.WriteLine("Ваше выражение: " + myOps);
+        }
+    }
+}
